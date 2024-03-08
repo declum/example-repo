8f0bb94c394532055b38f981d6626bcae271c8c3
Clement <declum@outlook.com>
2024-03-08 23:00:59 +0530

    Multi Tenant Menu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Team;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,22 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $user = User::firstOrCreate(
+            ['email' =>  'admin@admin.dev'],
+            ['name' => request('name'), 'password' => Hash::make('password'), 'email_verified_at' => now()]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $teams = [];
+        for( $i = 0; $i < 100; $i++ ) {
+            $team = Team::firstOrCreate(
+                ['name' =>  'Team ' . $i],
+                ['name' => 'Team ' . $i]
+            );  
+            array_push($teams, $team);
+        }
+
+        foreach( $teams as $team ) {
+            $team->users()->save($user);
+        }
     }
 }
